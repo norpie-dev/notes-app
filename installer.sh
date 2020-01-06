c6da5546e986d9f7e7cb68a59926c16b39e19338
BuildTools <unconfigured@null.spigotmc.org>
2020-01-06 16:32:32 +0100

    fixed installer

--- installer.sh
+++ installer.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
-NOTESDIR=~/Notes
-COMPILEDNOTESDIR=~/Compiled\ Notes
+NOTESDIR=/home/$SUDO_USER/Notes
+COMPILEDNOTESDIR=/home/$SUDO_USER/Compiled\ Notes
 
-FONTSDIR=~/.notes_data/
+FONTSDIR=/home/$SUDO_USER/.notes_data/
 
 if [[ $EUID -ne 0 ]]; then
     echo "this script should run as root"
     exit 1
 fi
 
-if [[ ! -e $NOTESDIR ]]; then
-    mkdir $NOTESDIR
-fi
-
-if [[ ! -e $COMPILEDNOTESDIR ]]; then
-    mkdir $COMPILEDNOTESDIR
-fi
-
-if [[ ! -e $FONTSDIR ]]; then
-    mkdir $FONTSDIR
-fi
-
 cp notes_app/notes.sh /bin/notes
 chmod +x /bin/notes
 
-cp fonts/font.tex $FONTSDIR
+if [[ ! -e $NOTESDIR ]]; then
+    mkdir "$NOTESDIR"
+fi
+
+if [[ ! -e $COMPILEDNOTESDIR ]]; then
+    mkdir "$COMPILEDNOTESDIR"
+fi
+
+if [[ ! -e $FONTSDIR ]]; then
+    mkdir "$FONTSDIR"
+fi
+
+chown "$SUDO_USER":"$SUDO_USER" /home/"$SUDO_USER"/*
+
+cp notes_app/fonts/font.tex $FONTSDIR
